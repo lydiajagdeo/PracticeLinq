ef4d44136f2ba2e02df1b813f25560e2b913deaf
“Lydia <lydiajagdeo@gmail.com>
2022-09-05 18:19:03 -0400

    PracticeLinq initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+List<string> makeUpBrands = new List<string>()
+{
+    "Mac",
+    "UrbanDecay",
+    "Fenty",
+    "Makeup Forever",
+    "NYX",
+    "Laura Mercier"
+
+};
+
+makeUpBrands.OrderBy(brand => brand.Length).ToList().ForEach(brand => Console.WriteLine(brand));
+
